b1b2cde4d0b072c3bc01d811e40d3cf2430b2126
Waheb26 <cherwaheb@gmail.com>
2023-03-28 12:16:35 +0200

    Hercules

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+const Fighter = require("./src/Fighter.js");
+
+const fighters = [
+    new Fighter ("Héraclès", 20, 6, 100 ),
+    new Fighter ("Lion de Némée", 11, 13, 100)
+];
+
+console.log(fighters[0], [3])
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,21 @@
 /* Fighter class definition */
+
+const MAX_LIFE = 100
+
+class Fighter {
+    constructor (name, strength, dexterity, life) {
+        this.name = name;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.life = life;
+    }
+
+     fight(defender) {
+        const damage = Math.floor(Math.random(this.strenght))
+        const dodge = Math.floor(damage - defender(this.dexterity))
+       defender.life = damage - dodge
+     }
+      
+}
+
+module.exports = Fighter
